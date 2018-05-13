c442c25a272a36e917dfaf0b9834e2c4d1c4a7fd
Adrian Davis <chuck.d.kc@gmail.com>
2018-05-13 02:18:55 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -5,10 +5,11 @@ def countdown(num)
   while num >= 1 
     puts "#{num} SECOND(S)!"
     num -=1
+    countdown_with_sleep
   end
   return "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(num)
-  
+  sleep(num)
 end
